3bd0577aea0b222bc4d7abdeea097b2d3332f671
Alexandr <bodryakstuff@gmauil.com>
2022-09-01 22:56:02 +0300

    3 задания готовы

---
+++ Task03/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine ("Введите число");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = (a % 2);
+if (b == 0)
+{
+Console.WriteLine ("Число чётное");
+}
+else 
+{
+Console.WriteLine ("Число нечётное");
+};
