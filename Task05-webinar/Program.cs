e24a822027b2e31d51e0e77c2085d7413aca96c0
Alexandr <bodryakstuff@gmauil.com>
2022-09-02 22:09:18 +0300

    вэбинар 02сентября

---
+++ Task05-webinar/Program.cs
@@ -0,0 +1,14 @@
+﻿// Console.WriteLine("Введите первое число");
+// int a = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите второе число");
+// int b = Convert.ToInt32(Console.ReadLine());
+
+// bool square(int a1, int b1)
+// {
+//     return (a1 * a1 == b1 || b1 * b1 == a1);
+// }
+
+// bool result = square(a, b);
+
+// if (result) Console.WriteLine($"Правда");
+// else Console.WriteLine($"Ложь");
