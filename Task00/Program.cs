8df5d36497a1b456aebb535919547cc25d30ace6
Alexandr <bodryakstuff@gmauil.com>
2022-09-02 06:40:14 +0300

    Исправил задачу 00

--- Task00/Program.cs
+++ Task00/Program.cs
@@ -2,11 +2,16 @@
 int a = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine ("Введите второе число");
 int b = Convert.ToInt32(Console.ReadLine());
-if (a > b)
+if (a == b)
+{
+     Console.WriteLine ("Числа равны");
+}
+else
+{if (a > b)
 {
     Console.WriteLine ("Первое число больше второго");
 }
 else 
 {
     Console.WriteLine ("Второе число больше первого");
-};
+}};
