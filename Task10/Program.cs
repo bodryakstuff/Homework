97c738b91500e8112005982cb3a6789d2e28e9f8
Alexandr <bodryakstuff@gmauil.com>
2022-09-05 21:41:24 +0300

    2 задания сделаны

---
+++ Task10/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.WriteLine("Введите число");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = a / 10 % 10;
+Console.WriteLine (b);
---
+++ Task13/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите число, соотвествующее дню недели (от 1 до 7)");
+Double a = Convert.ToDouble(Console.ReadLine());
+if (0 >= a | 7 < a) Console.WriteLine ("Число не соответствует дню недели");
+// if (7 < a) Console.WriteLine ("Число не соответствует дню недели");
+if (a > 0 & a < 6) 
+{
+    Console.WriteLine ("Число соответствует рабочему дню недели");
+}
+else
+{
+ Console.WriteLine ("Число соответствует выходному дню недели");
+}
