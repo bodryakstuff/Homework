d3147da61c4c409ab70e7f9141e793659a92dbab
Alexandr <bodryakstuff@gmauil.com>
2022-09-06 20:12:45 +0300

    Перепутаны 13 и 15 задания

---
+++ Task15/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите число");
+int a = Convert.ToInt32(Console.ReadLine());
+if (a < 100)
+      {
+        Console.WriteLine("Третьего числа нету");
+      }    
+ else
+ {
+    int b = 0;
+    do
+    {
+        b = a / 10;
+    }
+    while (b > 1000);
+    int c = b % 10;
+    Console.WriteLine($"Третее число {c}");
+ }
