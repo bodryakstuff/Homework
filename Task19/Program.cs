2f7bf7107a96d14680f1c681d8e5579e027d43df
Alexandr <bodryakstuff@gmauil.com>
2022-09-08 21:09:15 +0300

    commit 08sept

---
+++ Task19/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.Clear();
+Console.Write("Пользователь, введите пятизначное число: ");
+int i = Convert.ToInt32(Console.ReadLine());
+if (i > 99999 || i <= 9999)
+{
+    Console.WriteLine("Ваше число не пятизначное");
+}
+else
+{
+    int a = i % 10;
+    int b = i % 10 % 10;
+    int d = i % 10 % 10 % 10 % 10;
+    int e = i % 10 % 10 % 10 % 10 % 10;
+    if (a == e && b == d)
+    {
+        Console.WriteLine("Число - палиндром");
+    }
+    else 
+    {
+        Console.WriteLine("Число - не палиндром");
+    }
+}
+Console.WriteLine(a, b, c, d);
