ba9c1ed6ddbcbeeb1fa5a5df752a001df952998e
Alexandr <bodryakstuff@gmauil.com>
2022-09-09 00:20:33 +0300

    исправил 19

--- Task19/Program.cs
+++ Task19/Program.cs
@@ -1,20 +1,20 @@
 ﻿Console.Clear();
 Console.Write("Пользователь, введите пятизначное число: ");
 int i = Convert.ToInt32(Console.ReadLine());
-int a = 1;
-int b = 1;
-int d = 1;
-int e = 1;
+// int a = 1;
+// int b = 1;
+// int d = 1;
+// int e = 1;
 if (i > 99999 || i <= 9999)
 {
     Console.WriteLine("Ваше число не пятизначное");
 }
 else
 {
-    a = i % 10;
-    b = i / 10 % 10;
-    d = i / 10 / 10 / 10 % 10;
-    e = i / 10 / 10 / 10 / 10 % 10;
+   int a = i % 10;
+   int b = i / 10 % 10;
+   int d = i / 10 / 10 / 10 % 10;
+   int e = i / 10 / 10 / 10 / 10 % 10;
     if (a == e && b == d)
     {
         Console.WriteLine("Число - палиндром");
