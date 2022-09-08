33716a2e9caf75fcac22f1e02e1cc2ebfa61b57b
Alexandr <bodryakstuff@gmauil.com>
2022-09-08 21:37:39 +0300

    исправил задание 10

--- Task10/Program.cs
+++ Task10/Program.cs
@@ -1,4 +1,8 @@
 ﻿Console.WriteLine("Введите число");
 int a = Convert.ToInt32(Console.ReadLine());
-int b = a / 10 % 10;
-Console.WriteLine (b);
+if (a > 99 && a < 1000)
+{
+    int b = a / 10 % 10;
+    Console.WriteLine(b);
+}
+else Console.WriteLine ("Введено не трёхзначное число");
