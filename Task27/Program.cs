713c20340cad83c16cfdc84a00c3070a54ea5129
Alexandr <bodryakstuff@gmauil.com>
2022-09-12 20:00:28 +0300

    сделал 27

---
+++ Task27/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine ("Введите число:");
+int c = Convert.ToInt32(Console.ReadLine());
+int a = c;
+int i = 0;
+for (i = 0; a > 0; a = a / 10)
+    {
+        i = i + (a % 10);
+    }
+Console.WriteLine ($"Сумма цифр числа {c} = {i}");
+
