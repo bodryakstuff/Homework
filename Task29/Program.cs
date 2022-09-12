523648d09286641eee9a3b9ac1fd98e6eb4000ab
Alexandr <bodryakstuff@gmauil.com>
2022-09-12 20:11:28 +0300

    сделал 29

---
+++ Task29/Program.cs
@@ -0,0 +1,24 @@
+﻿int[] arr = new int[8];
+
+void FillArray(int[] array)
+{
+    int count = array.Length;
+    for (int i = 0; i < count; i++)
+    {
+        array[i] = new Random().Next(0, 999);
+    }
+}
+
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+    Console.Write("[");
+    for (int i = 0; i < count; i++)
+    {
+        if (i == count - 1) Console.Write($"{array[i]}]");
+        else Console.Write($"{array[i]}, ");
+    }
+}
+
+FillArray(arr);
+PrintArray(arr);
