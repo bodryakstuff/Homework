bbd5a5d9a1233af82ca8689f0839c242e559cc2b
Alexandr <bodryakstuff@gmauil.com>
2022-09-19 21:49:31 +0300

    наверное сделал 41

--- Seminary/Program.cs
+++ Seminary/Program.cs
@@ -367,3 +367,157 @@ double[] CreateArrayRndDouble (int size, int min, int max)
 
 
 
+void ReverseArrayFor (int[] array)
+{
+    int size = array.Length;
+
+    for (int i = 0; i < size/2; i++)
+    {
+        int temp = array[i];
+        array[i] = array[size - 1 - i];
+        array[size - 1 - i] = temp;
+    }
+}
+
+
+
+
+
+
+void ReverseArray (int[] array)
+{
+    int size = array.Length;
+    int index1 = 0;
+    int index2 = size - 1;
+
+    while (index1 < index2)
+    {
+        int obj = array[index1];
+        array[index1] = array[index2];
+        array[index2] = obj;
+
+        index1++;
+        index2--;
+    }
+}
+
+
+
+
+
+// Задача 42: Напишите программу, которая будет преобразовывать
+// десятичное число в двоичное.
+// 45 -> 101101
+// 3 -> 11
+// 2 -> 10
+
+Console.Clear();
+int num = default;
+Console.Write("Введите целое число: ");
+    while (!int.TryParse(Console.ReadLine(), out num))
+    {
+        Console.Write("Ошибка ввода. \nВведите целое число: ");
+    }
+
+int ConvertNumber(int number)
+{
+    int result = 0;
+    int count = 1;
+    while (number != 0)
+    {
+        result += (number % 2) * count;
+        number /= 2;
+        count*= 10;
+    }
+    return result;
+}
+
+Console.Write(ConvertNumber(num));
+
+
+
+// Задача 45: Напишите программу, которая будет создавать
+// копию заданного массива с помощью поэлементного
+// копирования.
+
+
+int[] CreateArrayRndInt(int size, int min, int max)
+{
+    int[] array = new int[size];
+    Random rnd = new Random();
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = rnd.Next(min, max + 1);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1) Console.Write($"{array[i]}, ");
+        else Console.Write($"{array[i]}");
+    }
+    Console.Write("]");
+}
+
+int[] CoppyArray(int[] array)
+{
+    int[] arr2 = new int [array.Length];
+    for (int i = 0; i < array.Length; i++)
+    {
+        arr2[i] = array[i];
+    }
+    return arr2;
+}
+
+int[] arr = CreateArrayRndInt(9,1,10);
+PrintArray(arr);
+Console.WriteLine();
+int[] arrcoppy = CoppyArray(arr);
+PrintArray(arrcoppy);
+
+
+
+
+// Задача 44: Не используя рекурсию, выведите первые N чисел
+// Фибоначчи. Первые два числа Фибоначчи: 0 и 1.
+// Если N = 5 -> 0 1 1 2 3
+// Если N = 3 -> 0 1 1
+// Если N = 7 -> 0 1 1 2 3 5 8
+// Задача 45: Напишите программу, которая будет создавать
+// копию заданного массива с помощью поэлементного
+// копирования.
+
+Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int[] Febonachi(int number)
+{
+    int[] array = new int [number];
+    array[0] = 0;
+    array[1] = 1;
+    for (int i = 2; i < number; i++)
+    {
+        array[i] = array[i-1] + array[i - 2];
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1) Console.Write($"{array[i]}, ");
+        else Console.Write($"{array[i]}");
+    }
+    Console.Write("]");
+}
+
+int[] arr = Febonachi(num);
+PrintArray(arr);
---
+++ Task41/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine ("Введите количество чисел");
+int a = Convert.ToInt32(Console.ReadLine());
+int[] arr = new int[a];
+int pos = 0;                                            //Переменная для подсчёта положительных чисел
+for (int i = 0; i < a; i++)
+{
+    Console.WriteLine ($"Введите число {i + 1}");       //Пишем так чтобы нумерация для пользователя шла с привычной единицы
+    arr[i] = Convert.ToInt32(Console.ReadLine());       // Вводим числа сразу в массив каждое в свой индекс
+    if (arr[i] > 0)                                     //Сравниваем с нулём
+    {
+        pos++;
+    }
+}
+Console.WriteLine ($"Количество положительных чисел = {pos}");
